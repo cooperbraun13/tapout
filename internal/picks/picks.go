3e79d196ddc75ab40b025bf22962851bd5b8fd30
cooperbraun13 <cooperjbraun13@gmail.com>
2026-01-12 03:04:29 -0800

    feat: setup picks.go file to get ready for future work. wrote a method on the Picks struct to save to our json files in picks/ and a function to load previous picks if users want to look back

--- internal/picks/picks.go
+++ internal/picks/picks.go
@@ -1,1 +1,18 @@
 package picks
+
+import "github.com/cooperbraun13/tapout/internal/event"
+
+type Picks struct {
+	EventName string
+	Results   map[int]event.Fighter // Maps fight to the winning fighter
+}
+
+// Save picks to json
+func (p *Picks) Save(path string) error {
+
+}
+
+// Function to load picks from json (for viewing past picks)
+func LoadPicks(path string) (*Picks, error) {
+
+}
--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,6 +1,9 @@
 package tui
 
-import "github.com/cooperbraun13/tapout/internal/event"
+import (
+	"github.com/cooperbraun13/tapout/internal/event"
+	"github.com/cooperbraun13/tapout/internal/picks"
+)
 
 type Screen int
 
@@ -11,10 +14,8 @@ const (
 )
 
 type Model struct {
-	Screen       Screen            // Which screen we are currently on
-	Events       []event.Event     // Events listed
-	CurrentEvent int               // Cursor on what event we are selecting
-	CurrentFight int               // Cursor on what fight we are selecting
-	Picks        map[string]string // Fight -> Winner
-
+	Screen       Screen        // Which screen we are currently on
+	Events       []event.Event // Events listed
+	Picks        picks.Picks   // User's selection
+	CurrentEvent int           // Cursor on what event we are selecting
 }
